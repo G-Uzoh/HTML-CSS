0c8f3c24cb35e2c70452b025057ea34c23a79e9c
George Uzoh <ugochukwuuzoh@gmail.com>
2023-09-30 16:30:03 +0300

    add scroll to top button

--- Portfolio_Project/main.js
+++ Portfolio_Project/main.js
@@ -1,7 +1,25 @@
-const button = document.querySelector('button');
+// Get DOM elements
+const button = document.querySelector('.send-btn');
+const toTopBtn = document.querySelector('.back-to-top');
 
 const handleClick = (e) => {
     e.preventDefault();
 }
 
+// Add scroll event to window
+window.onscroll = function() {scrollFn()};
+ 
+// Define the scroll function
+const scrollFn = () => {
+    // Add responsiveness for different browsers
+    document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000 ? toTopBtn.style.display = 'block' : toTopBtn.style.display = 'none';
+}
+
+const backToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Add event listeners to DOM elements
 button.addEventListener('click', handleClick);
+toTopBtn.addEventListener('click', backToTop);
